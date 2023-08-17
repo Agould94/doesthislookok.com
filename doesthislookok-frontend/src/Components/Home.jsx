0962bc1677576e01854b289d3154ab1d52b58686
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-08-17 12:47:20 -0700

    Add to Frontend

---
+++ doesthislookok-frontend/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className = "welcome">Welcome to Doesthislookok.com, the place where you can come to ask, does this look ok?</div>
+  )
+}
+
+export default Home
