4a89140777233ee8ac76a525fda2c5df5d767c81
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-08-17 13:17:38 -0700

    add react router and redux

--- doesthislookok-frontend/src/Components/Home.jsx
+++ doesthislookok-frontend/src/Components/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className = "welcome">Welcome to Doesthislookok.com, the place where you can come to ask, does this look ok?</div>
+    <div>
+        <h1 className = "welcome">Welcome to Doesthislookok.com, the place where you can come to ask, does this look ok?</h1>
+        <button>Check if it looks ok</button>
+    </div>
+  
   )
 }
 
