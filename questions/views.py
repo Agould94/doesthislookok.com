1114fed41b24eaa00988b5ea73f85e7122ba1c08
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-08-21 15:22:03 -0700

    add json parsing to question model

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.core import serializers
 from django.http import HttpResponse
+from django.http import JsonResponse
+from questions.models import Question, Mark, User
 
 def index(request):
-    return HttpResponse("Hello, world, you're at the image and question upload index.")
+    all_questions = Question.objects.all()
+    json_questions = serializers.serialize('json', all_questions)
+    return JsonResponse(json_questions, safe=False)
+
+
+    
 
 # Create your views here.
