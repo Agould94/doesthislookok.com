477787974e5a9bbb7b044875dfb2116ed3e6649a
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-09-01 23:53:42 -0700

    add point and click functionality

--- doesthislookok-frontend/src/features/Intake/HumanBody.jsx
+++ doesthislookok-frontend/src/features/Intake/HumanBody.jsx
@@ -2,17 +2,31 @@ import React, {useState} from 'react'
 import humanBody from "../../media/HumanBody.png"
 
 function HumanBody() {
+  const [point, setPoint] = useState(null)
 
     function handleBodyClick(e){
-        const xCoordinate = e.nativeEvent.offsetX;
-        const yCoordinate = e.nativeEvent.offsetY;
-        console.log(`X: ${xCoordinate}, Y: ${yCoordinate}`)
+        const x = e.nativeEvent.offsetX;
+        const y = e.nativeEvent.offsetY;
+
+        const newPoint = {x: x,y: y}
+
+        setPoint(newPoint)
+        console.log(newPoint)
+        console.log(`X: ${x}, Y: ${y}`)
       }
 
 
   return (
-    <div>
-    <img src={humanBody} className = "body-img" onClick={handleBodyClick}></img>
+    <div className = "outerdiv">
+      <div className="body-img-div">
+      <img src={humanBody} className = "body-img" onClick={handleBodyClick}></img>
+        {point ?
+          <div className = "point" style={{position: 'absolute', left: `${point.x}px`, top: `${point.y}px`}}>
+          </div>
+          :
+          <div></div>
+        }
+      </div>
     </div>
   )
 }
